ad1dfaa32d20a0b2c486ed74ef3ddd3364b24ab0
Tom de Geus <tom@geus.me>
2020-09-13 19:10:04 +0200

    Adding/updating benchmarks

--- examples/benchmark.cpp
+++ examples/benchmark.cpp
@@ -4,16 +4,17 @@ namespace GMat = GMatElastoPlasticQPot::Cartesian2d;
 
 int main()
 {
-    xt::xtensor<double,1> epsy = xt::linspace(0, 1, 20001);
+    xt::xtensor<double,1> epsy = xt::linspace<double>(1e-5, 1, 20001);
     GMat::Tensor2 Eps = xt::zeros<double>({2, 2});
     GMat::Tensor2 Sig = xt::zeros<double>({2, 2});
 
     GMat::Cusp cusp(1.0, 1.0, epsy);
 
     for (size_t i = 0; i < 100000; ++i) {
-        Eps(0, 1) = 0.5 / 100000.0 * static_cast<double>(i);
-        Eps(1, 0) = 0.5 / 100000.0 * static_cast<double>(i);
-        cusp.stress(Eps, Sig);
+        Eps(0, 1) = 0.5 / 100000.0 * static_cast<double>(i + 1);
+        Eps(1, 0) = 0.5 / 100000.0 * static_cast<double>(i + 1);
+        cusp.setStrain(Eps);
+        cusp.stress(Sig);
     }
 
     return 0;
