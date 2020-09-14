0731a73fd54f7871551a2d3e9997735c9e89d344
Tom de Geus <tom@geus.me>
2020-09-14 12:07:30 +0200

    Finishing tests

--- test/Cartesian2d.py
+++ test/Cartesian2d.py
@@ -2,7 +2,7 @@
 import GMatElastoPlasticQPot.Cartesian2d as GMat
 import numpy as np
 
-def EQ(a, b):
+def CLOSE(a, b):
   assert np.abs(a-b) < 1.e-12
 
 K = 12.3
@@ -15,39 +15,42 @@ Eps = np.array(
     [[epsm, gamma],
      [gamma, epsm]])
 
-# Elastic
+# Elastic - stress
 
 mat = GMat.Elastic(K, G)
-Sig = mat.Stress(Eps)
+mat.setStrain(Eps)
+Sig = mat.Stress()
 
-EQ(Sig[0,0], K * epsm)
-EQ(Sig[1,1], K * epsm)
-EQ(Sig[0,1], G * gamma)
-EQ(Sig[1,0], G * gamma)
+CLOSE(Sig[0,0], K * epsm)
+CLOSE(Sig[1,1], K * epsm)
+CLOSE(Sig[0,1], G * gamma)
+CLOSE(Sig[1,0], G * gamma)
 
-# Cusp
+# Cusp - stress
 
 mat = GMat.Cusp(K, G, [0.01, 0.03, 0.10])
-Sig = mat.Stress(Eps)
+mat.setStrain(Eps)
+Sig = mat.Stress()
 
-EQ(Sig[0,0], K * epsm)
-EQ(Sig[1,1], K * epsm)
-EQ(Sig[0,1], G * 0.0)
-EQ(Sig[1,0], G * 0.0)
-EQ(mat.epsp(Eps), 0.02)
-EQ(mat.find(Eps), 1)
+CLOSE(Sig[0,0], K * epsm)
+CLOSE(Sig[1,1], K * epsm)
+CLOSE(Sig[0,1], G * 0.0)
+CLOSE(Sig[1,0], G * 0.0)
+CLOSE(mat.epsp(Eps), 0.02)
+CLOSE(mat.find(Eps), 1)
 
-# Smooth
+# Smooth - stress
 
 mat = GMat.Smooth(K, G, [0.01, 0.03, 0.10])
-Sig = mat.Stress(Eps)
+mat.setStrain(Eps)
+Sig = mat.Stress()
 
-EQ(Sig[0,0], K * epsm)
-EQ(Sig[1,1], K * epsm)
-EQ(Sig[0,1], G * 0.0)
-EQ(Sig[1,0], G * 0.0)
-EQ(mat.epsp(Eps), 0.02)
-EQ(mat.find(Eps), 1)
+CLOSE(Sig[0,0], K * epsm)
+CLOSE(Sig[1,1], K * epsm)
+CLOSE(Sig[0,1], G * 0.0)
+CLOSE(Sig[1,0], G * 0.0)
+CLOSE(mat.epsp(Eps), 0.02)
+CLOSE(mat.find(Eps), 1)
 
 # Matrix
 
@@ -72,27 +75,28 @@ for i in range(2):
     for j in range(2):
         eps[:, :, i, j] = Eps[i, j]
 
-sig = mat.Stress(eps)
-epsp = mat.Epsp(eps)
+mat.setStrain(eps)
+sig = mat.Stress()
+epsp = mat.Epsp()
 
 for q in range(nip):
 
-    EQ(sig[0,q,0,0], K * epsm)
-    EQ(sig[0,q,1,1], K * epsm)
-    EQ(sig[0,q,0,1], G * gamma)
-    EQ(sig[0,q,0,1], G * gamma)
-    EQ(epsp[0,q], 0.0)
+    CLOSE(sig[0,q,0,0], K * epsm)
+    CLOSE(sig[0,q,1,1], K * epsm)
+    CLOSE(sig[0,q,0,1], G * gamma)
+    CLOSE(sig[0,q,0,1], G * gamma)
+    CLOSE(epsp[0,q], 0.0)
 
-    EQ(sig[1,q,0,0], K * epsm)
-    EQ(sig[1,q,1,1], K * epsm)
-    EQ(sig[1,q,0,1], G * 0.0)
-    EQ(sig[1,q,0,1], G * 0.0)
-    EQ(epsp[1,q], gamma)
+    CLOSE(sig[1,q,0,0], K * epsm)
+    CLOSE(sig[1,q,1,1], K * epsm)
+    CLOSE(sig[1,q,0,1], G * 0.0)
+    CLOSE(sig[1,q,0,1], G * 0.0)
+    CLOSE(epsp[1,q], gamma)
 
-    EQ(sig[2,q,0,0], K * epsm)
-    EQ(sig[2,q,1,1], K * epsm)
-    EQ(sig[2,q,0,1], G * 0.0)
-    EQ(sig[2,q,0,1], G * 0.0)
-    EQ(epsp[2,q], gamma)
+    CLOSE(sig[2,q,0,0], K * epsm)
+    CLOSE(sig[2,q,1,1], K * epsm)
+    CLOSE(sig[2,q,0,1], G * 0.0)
+    CLOSE(sig[2,q,0,1], G * 0.0)
+    CLOSE(epsp[2,q], gamma)
 
 print('All checks passed')
