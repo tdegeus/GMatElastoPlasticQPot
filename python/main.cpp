18a880f5e927c814984b5e0fcf77fab4ac7fac1c
Tom de Geus <tom@geus.me>
2020-11-23 11:59:24 +0100

    Shortening Python API generator

--- python/main.cpp
+++ python/main.cpp
@@ -16,50 +16,34 @@
 
 namespace py = pybind11;
 
-template <size_t rank, class T>
-auto add_common_members_array(T& self)
+template <class S, class T>
+auto construct_Array(T& self)
 {
-    namespace SM = GMatElastoPlasticQPot::Cartesian2d;
+    self.def(py::init<std::array<size_t, S::rank>>(), "Array of material points.", py::arg("shape"))
 
-    self.def(py::init<std::array<size_t, rank>>(), "Array of material points.", py::arg("shape"))
-
-        .def("shape", &SM::Array<rank>::shape, "Shape of array.")
-
-        .def("K", &SM::Array<rank>::K, "Array with bulk moduli.")
-
-        .def("G", &SM::Array<rank>::G, "Array with shear moduli.")
-
-        .def("I2", &SM::Array<rank>::I2, "Array with 2nd-order unit tensors.")
-
-        .def("II", &SM::Array<rank>::II, "Array with 4th-order tensors = dyadic(I2, I2).")
-
-        .def("I4", &SM::Array<rank>::I4, "Array with 4th-order unit tensors.")
-
-        .def("I4rt", &SM::Array<rank>::I4rt, "Array with 4th-order right-transposed unit tensors.")
-
-        .def("I4s", &SM::Array<rank>::I4s, "Array with 4th-order symmetric projection tensors.")
-
-        .def("I4d", &SM::Array<rank>::I4d, "Array with 4th-order deviatoric projection tensors.")
-
-        .def("type", &SM::Array<rank>::type, "Array with material types.")
-
-        .def("isElastic", &SM::Array<rank>::isElastic, "Boolean-matrix: true for Elastic.")
-
-        .def("isPlastic", &SM::Array<rank>::isPlastic, "Boolean-matrix: true for Cusp/Smooth.")
-
-        .def("isCusp", &SM::Array<rank>::isCusp, "Boolean-matrix: true for Cusp.")
-
-        .def("isSmooth", &SM::Array<rank>::isSmooth, "Boolean-matrix: true for Smooth.")
-
-        .def("check", &SM::Array<rank>::check, "Throws if any unset point is found.")
+        .def("shape", &S::shape, "Shape of array.")
+        .def("I2", &S::I2, "Array with 2nd-order unit tensors.")
+        .def("II", &S::II, "Array with 4th-order tensors = dyadic(I2, I2).")
+        .def("I4", &S::I4, "Array with 4th-order unit tensors.")
+        .def("I4rt", &S::I4rt, "Array with 4th-order right-transposed unit tensors.")
+        .def("I4s", &S::I4s, "Array with 4th-order symmetric projection tensors.")
+        .def("I4d", &S::I4d, "Array with 4th-order deviatoric projection tensors.")
+        .def("K", &S::K, "Array with bulk moduli.")
+        .def("G", &S::G, "Array with shear moduli.")
+        .def("type", &S::type, "Array with material types.")
+        .def("isElastic", &S::isElastic, "Boolean-matrix: true for Elastic.")
+        .def("isPlastic", &S::isPlastic, "Boolean-matrix: true for Cusp/Smooth.")
+        .def("isCusp", &S::isCusp, "Boolean-matrix: true for Cusp.")
+        .def("isSmooth", &S::isSmooth, "Boolean-matrix: true for Smooth.")
+        .def("check", &S::check, "Throws if any unset point is found.")
 
         .def(
             "setElastic",
             py::overload_cast<
-                const xt::xtensor<size_t, rank>&,
-                const xt::xtensor<size_t, rank>&,
+                const xt::xtensor<size_t, S::rank>&,
+                const xt::xtensor<size_t, S::rank>&,
                 const xt::xtensor<double, 1>&,
-                const xt::xtensor<double, 1>&>(&SM::Array<rank>::setElastic),
+                const xt::xtensor<double, 1>&>(&S::setElastic),
             "Set specific entries 'Elastic'.",
             py::arg("I"),
             py::arg("idx"),
@@ -69,12 +53,12 @@ auto add_common_members_array(T& self)
         .def(
             "setCusp",
             py::overload_cast<
-                const xt::xtensor<size_t, rank>&,
-                const xt::xtensor<size_t, rank>&,
+                const xt::xtensor<size_t, S::rank>&,
+                const xt::xtensor<size_t, S::rank>&,
                 const xt::xtensor<double, 1>&,
                 const xt::xtensor<double, 1>&,
                 const xt::xtensor<double, 2>&,
-                bool>(&SM::Array<rank>::setCusp),
+                bool>(&S::setCusp),
             "Set specific entries 'Cusp'.",
             py::arg("I"),
             py::arg("idx"),
@@ -86,12 +70,12 @@ auto add_common_members_array(T& self)
         .def(
             "setSmooth",
             py::overload_cast<
-                const xt::xtensor<size_t, rank>&,
-                const xt::xtensor<size_t, rank>&,
+                const xt::xtensor<size_t, S::rank>&,
+                const xt::xtensor<size_t, S::rank>&,
                 const xt::xtensor<double, 1>&,
                 const xt::xtensor<double, 1>&,
                 const xt::xtensor<double, 2>&,
-                bool>(&SM::Array<rank>::setSmooth),
+                bool>(&S::setSmooth),
             "Set specific entries 'Smooth'.",
             py::arg("I"),
             py::arg("idx"),
@@ -102,8 +86,8 @@ auto add_common_members_array(T& self)
 
         .def(
             "setElastic",
-            py::overload_cast<const xt::xtensor<size_t, rank>&, double, double>(
-                &SM::Array<rank>::setElastic),
+            py::overload_cast<const xt::xtensor<size_t, S::rank>&, double, double>(
+                &S::setElastic),
             "Set specific entries 'Elastic'.",
             py::arg("I"),
             py::arg("K"),
@@ -112,11 +96,11 @@ auto add_common_members_array(T& self)
         .def(
             "setCusp",
             py::overload_cast<
-                const xt::xtensor<size_t, rank>&,
+                const xt::xtensor<size_t, S::rank>&,
                 double,
                 double,
                 const xt::xtensor<double, 1>&,
-                bool>(&SM::Array<rank>::setCusp),
+                bool>(&S::setCusp),
             "Set specific entries 'Cusp'.",
             py::arg("I"),
             py::arg("K"),
@@ -127,11 +111,11 @@ auto add_common_members_array(T& self)
         .def(
             "setSmooth",
             py::overload_cast<
-                const xt::xtensor<size_t, rank>&,
+                const xt::xtensor<size_t, S::rank>&,
                 double,
                 double,
                 const xt::xtensor<double, 1>&,
-                bool>(&SM::Array<rank>::setSmooth),
+                bool>(&S::setSmooth),
             "Set specific entries 'Smooth'.",
             py::arg("I"),
             py::arg("K"),
@@ -139,62 +123,72 @@ auto add_common_members_array(T& self)
             py::arg("epsy"),
             py::arg("init_elastic") = true)
 
-        .def(
-            "setStrain",
-            &SM::Array<rank>::setStrain,
-            "Set matrix of strain tensors.",
-            py::arg("Eps"))
-
-        .def(
-            "Stress",
-            &SM::Array<rank>::Stress,
-            "Returns matrix of stress tensors, given the current strain.")
-
-        .def(
-            "Tangent",
-            &SM::Array<rank>::Tangent,
-            "Returns matrices of stress tangent stiffness tensors, given the current strain.")
-
-        .def(
-            "CurrentIndex",
-            &SM::Array<rank>::CurrentIndex,
-            "Returns matrix of potential indices, given the current strain.")
-
-        .def(
-            "CurrentYieldLeft",
-            &SM::Array<rank>::CurrentYieldLeft,
-            "Returns matrix of yield strains to the left, given the current strain.")
-
-        .def(
-            "CurrentYieldRight",
-            &SM::Array<rank>::CurrentYieldRight,
-            "Returns matrix of yield strains to the left, given the current strain.")
+        .def("setStrain", &S::setStrain, "Set strain tensors.", py::arg("Eps"))
+        .def("Stress", &S::Stress, "Get stress tensors.")
+        .def("Tangent", &S::Tangent, "Get stiffness tensors.")
+        .def("CurrentIndex", &S::CurrentIndex, "Get potential indices.")
+        .def("CurrentYieldLeft", &S::CurrentYieldLeft, "Get left yield strains.")
+        .def("CurrentYieldRight", &S::CurrentYieldRight, "Get right yield strains.")
+        .def("Epsp", &S::Epsp, "Get equivalent plastic strains.")
+        .def("Energy", &S::Energy, "Get energies.")
 
         .def(
             "checkYieldBoundLeft",
-            &SM::Array<rank>::checkYieldBoundLeft,
+            &S::checkYieldBoundLeft,
             "Check that 'the particle' is at least 'n' wells from the far-left.",
             py::arg("n") = 0)
 
         .def(
             "checkYieldBoundRight",
-            &SM::Array<rank>::checkYieldBoundRight,
+            &S::checkYieldBoundRight,
             "Check that 'the particle' is at least 'n' wells from the far-right.",
             py::arg("n") = 0)
 
-        .def(
-            "Epsp",
-            &SM::Array<rank>::Epsp,
-            "Returns matrix of equivalent plastic strains, given the current strain.")
-
-        .def(
-            "Energy",
-            &SM::Array<rank>::Energy,
-            "Returns matrix of energies, given the current strain.")
-
-        .def("__repr__", [](const SM::Array<rank>&) {
+        .def("__repr__", [](const S&) {
             return "<GMatElastoPlasticQPot.Cartesian2d.Array>";
         });
+}
+
+template <class S, class T>
+void add_deviatoric_overloads(T& module)
+{
+    module.def(
+        "Deviatoric",
+        static_cast<S (*)(const S&)>(&GMatElastoPlasticQPot::Cartesian2d::Deviatoric<S>),
+        "Deviatoric part of a(n) (array of) tensor(s).",
+        py::arg("A"));
+}
+
+template <class R, class S, class T>
+void add_hydrostatic_overloads(T& module)
+{
+    module.def(
+        "Hydrostatic",
+        static_cast<R (*)(const S&)>(&GMatElastoPlasticQPot::Cartesian2d::Hydrostatic<S>),
+        "Hydrostatic part of a(n) (array of) tensor(s).",
+        py::arg("A"));
+}
+
+template <class R, class S, class T>
+void add_epsd_overloads(T& module)
+{
+    module.def(
+        "Epsd",
+        static_cast<R (*)(const S&)>(
+            &GMatElastoPlasticQPot::Cartesian2d::Epsd<S>),
+        "Equivalent strain of a(n) (array of) tensor(s).",
+        py::arg("A"));
+}
+
+template <class R, class S, class T>
+void add_sigd_overloads(T& module)
+{
+    module.def(
+        "Sigd",
+        static_cast<R (*)(const S&)>(
+            &GMatElastoPlasticQPot::Cartesian2d::Sigd<S>),
+        "Equivalent stress of a(n) (array of) tensor(s).",
+        py::arg("A"));
 }
 
 PYBIND11_MODULE(GMatElastoPlasticQPot, m)
@@ -221,89 +215,18 @@ PYBIND11_MODULE(GMatElastoPlasticQPot, m)
 
     // Tensor algebra
 
-    sm.def(
-        "Deviatoric",
-        static_cast<xt::xtensor<double, 4> (*)(const xt::xtensor<double, 4>&)>(
-            &SM::Deviatoric<xt::xtensor<double, 4>>),
-        "Deviatoric part of a 2nd-order tensor. Returns matrix of 2nd-order tensors.",
-        py::arg("A"));
-
-    sm.def(
-        "Deviatoric",
-        static_cast<xt::xtensor<double, 3> (*)(const xt::xtensor<double, 3>&)>(
-            &SM::Deviatoric<xt::xtensor<double, 3>>),
-        "Deviatoric part of a 2nd-order tensor. Returns list of 2nd-order tensors.",
-        py::arg("A"));
-
-    sm.def(
-        "Deviatoric",
-        static_cast<xt::xtensor<double, 2> (*)(const xt::xtensor<double, 2>&)>(
-            &SM::Deviatoric<xt::xtensor<double, 2>>),
-        "Deviatoric part of a 2nd-order tensor. Returns 2nd-order tensor.",
-        py::arg("A"));
-
-    sm.def(
-        "Hydrostatic",
-        static_cast<xt::xtensor<double, 2> (*)(const xt::xtensor<double, 4>&)>(
-            &SM::Hydrostatic<xt::xtensor<double, 4>>),
-        "Hydrostatic part of a 2nd-order tensor. Returns matrix (of scalars).",
-        py::arg("A"));
-
-    sm.def(
-        "Hydrostatic",
-        static_cast<xt::xtensor<double, 1> (*)(const xt::xtensor<double, 3>&)>(
-            &SM::Hydrostatic<xt::xtensor<double, 3>>),
-        "Hydrostatic part of a 2nd-order tensor. Returns list (of scalars).",
-        py::arg("A"));
-
-    sm.def(
-        "Hydrostatic",
-        static_cast<xt::xtensor<double, 0> (*)(const xt::xtensor<double, 2>&)>(
-            &SM::Hydrostatic<xt::xtensor<double, 2>>),
-        "Hydrostatic part of a 2nd-order tensor. Returns scalar.",
-        py::arg("A"));
-
-    sm.def(
-        "Epsd",
-        static_cast<xt::xtensor<double, 2> (*)(const xt::xtensor<double, 4>&)>(
-            &SM::Epsd<xt::xtensor<double, 4>>),
-        "Equivalent strain. Returns matrix (of scalars).",
-        py::arg("A"));
-
-    sm.def(
-        "Epsd",
-        static_cast<xt::xtensor<double, 1> (*)(const xt::xtensor<double, 3>&)>(
-            &SM::Epsd<xt::xtensor<double, 3>>),
-        "Equivalent strain. Returns list (of scalars).",
-        py::arg("A"));
-
-    sm.def(
-        "Epsd",
-        static_cast<xt::xtensor<double, 0> (*)(const xt::xtensor<double, 2>&)>(
-            &SM::Epsd<xt::xtensor<double, 2>>),
-        "Equivalent strain. Returns scalar.",
-        py::arg("A"));
-
-    sm.def(
-        "Sigd",
-        static_cast<xt::xtensor<double, 2> (*)(const xt::xtensor<double, 4>&)>(
-            &SM::Sigd<xt::xtensor<double, 4>>),
-        "Equivalent stress. Returns matrix (of scalars).",
-        py::arg("A"));
-
-    sm.def(
-        "Sigd",
-        static_cast<xt::xtensor<double, 1> (*)(const xt::xtensor<double, 3>&)>(
-            &SM::Sigd<xt::xtensor<double, 3>>),
-        "Equivalent stress. Returns list (of scalars).",
-        py::arg("A"));
-
-    sm.def(
-        "Sigd",
-        static_cast<xt::xtensor<double, 0> (*)(const xt::xtensor<double, 2>&)>(
-            &SM::Sigd<xt::xtensor<double, 2>>),
-        "Equivalent stress. Returns scalar.",
-        py::arg("A"));
+    add_deviatoric_overloads<xt::xtensor<double, 4>>(sm);
+    add_deviatoric_overloads<xt::xtensor<double, 3>>(sm);
+    add_deviatoric_overloads<xt::xtensor<double, 2>>(sm);
+    add_hydrostatic_overloads<xt::xtensor<double, 2>, xt::xtensor<double, 4>>(sm);
+    add_hydrostatic_overloads<xt::xtensor<double, 1>, xt::xtensor<double, 3>>(sm);
+    add_hydrostatic_overloads<xt::xtensor<double, 0>, xt::xtensor<double, 2>>(sm);
+    add_epsd_overloads<xt::xtensor<double, 2>, xt::xtensor<double, 4>>(sm);
+    add_epsd_overloads<xt::xtensor<double, 1>, xt::xtensor<double, 3>>(sm);
+    add_epsd_overloads<xt::xtensor<double, 0>, xt::xtensor<double, 2>>(sm);
+    add_sigd_overloads<xt::xtensor<double, 2>, xt::xtensor<double, 4>>(sm);
+    add_sigd_overloads<xt::xtensor<double, 1>, xt::xtensor<double, 3>>(sm);
+    add_sigd_overloads<xt::xtensor<double, 0>, xt::xtensor<double, 2>>(sm);
 
     // Material point: Elastic
 
@@ -312,11 +235,8 @@ PYBIND11_MODULE(GMatElastoPlasticQPot, m)
         .def(py::init<double, double>(), "Elastic material point.", py::arg("K"), py::arg("G"))
 
         .def("K", &SM::Elastic::K, "Returns the bulk modulus.")
-
         .def("G", &SM::Elastic::G, "Returns the shear modulus.")
-
         .def("setStrain", &SM::Elastic::setStrain<SM::Tensor2>, "Set current strain tensor.")
-
         .def("Stress", &SM::Elastic::Stress, "Returns stress tensor, for last known strain.")
 
         .def(
@@ -343,13 +263,9 @@ PYBIND11_MODULE(GMatElastoPlasticQPot, m)
             py::arg("init_elastic") = true)
 
         .def("K", &SM::Cusp::K, "Returns the bulk modulus.")
-
         .def("G", &SM::Cusp::G, "Returns the shear modulus.")
-
         .def("epsy", &SM::Cusp::epsy, "Returns the yield strains.")
-
         .def("setStrain", &SM::Cusp::setStrain<SM::Tensor2>, "Set current strain tensor.")
-
         .def("Stress", &SM::Cusp::Stress, "Returns stress tensor, for last known strain.")
 
         .def(
@@ -406,13 +322,9 @@ PYBIND11_MODULE(GMatElastoPlasticQPot, m)
             py::arg("init_elastic") = true)
 
         .def("K", &SM::Smooth::K, "Returns the bulk modulus.")
-
         .def("G", &SM::Smooth::G, "Returns the shear modulus.")
-
         .def("epsy", &SM::Smooth::epsy, "Returns the yield strains.")
-
         .def("setStrain", &SM::Smooth::setStrain<SM::Tensor2>, "Set current strain tensor.")
-
         .def("Stress", &SM::Smooth::Stress, "Returns stress tensor, for last known strain.")
 
         .def(
@@ -472,11 +384,10 @@ PYBIND11_MODULE(GMatElastoPlasticQPot, m)
     // Array
 
     py::class_<SM::Array<1>> array1d(sm, "Array1d");
-    add_common_members_array<1>(array1d);
-
     py::class_<SM::Array<2>> array2d(sm, "Array2d");
-    add_common_members_array<2>(array2d);
-
     py::class_<SM::Array<3>> array3d(sm, "Array3d");
-    add_common_members_array<3>(array3d);
+
+    construct_Array<SM::Array<1>>(array1d);
+    construct_Array<SM::Array<2>>(array2d);
+    construct_Array<SM::Array<3>>(array3d);
 }
