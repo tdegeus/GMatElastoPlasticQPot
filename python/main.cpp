fddccf6d878fc67b9da1a8c2f8890239581499cf
Tom de Geus <tom@geus.me>
2022-02-18 17:11:39 +0100

    Improving Python naming

--- python/main.cpp
+++ python/main.cpp
@@ -15,6 +15,28 @@
 
 namespace py = pybind11;
 
+/**
+Overrides the `__name__` of a module.
+Classes defined by pybind11 use the `__name__` of the module as of the time they are defined,
+which affects the `__repr__` of the class type objects.
+*/
+class ScopedModuleNameOverride {
+public:
+    explicit ScopedModuleNameOverride(py::module m, std::string name) : module_(std::move(m))
+    {
+        original_name_ = module_.attr("__name__");
+        module_.attr("__name__") = name;
+    }
+    ~ScopedModuleNameOverride()
+    {
+        module_.attr("__name__") = original_name_;
+    }
+
+private:
+    py::module module_;
+    py::object original_name_;
+};
+
 template <class S, class T>
 auto construct_Array(T& cls)
 {
@@ -382,6 +404,10 @@ void add_sigd(M& mod)
 
 PYBIND11_MODULE(_GMatElastoPlasticQPot, m)
 {
+    // Ensure members to display as `GMatElastoPlasticQPot.X`
+    // (not `GMatElastoPlasticQPot._GMatElastoPlasticQPot.X`)
+    ScopedModuleNameOverride name_override(m, "GMatElastoPlasticQPot");
+
     xt::import_numpy();
 
     m.doc() = "Elasto-plastic material model";
