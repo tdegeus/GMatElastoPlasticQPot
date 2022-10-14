d78578693874c0c33ae6e13e593dc861d5bad553
Tom de Geus <tom@geus.me>
2022-10-14 17:46:29 +0200

    Improving return type (lead to segfault with hardware optimisation on some platforms

--- include/GMatElastoPlasticQPot/Cartesian2d.h
+++ include/GMatElastoPlasticQPot/Cartesian2d.h
@@ -49,7 +49,10 @@ inline auto Epsd(const T& A) -> typename GMatTensor::allocate<xt::get_rank<T>::v
     GMATELASTOPLASTICQPOT_ASSERT(A.shape(A.dimension() - 1) == 2);
     GMATELASTOPLASTICQPOT_ASSERT(A.shape(A.dimension() - 2) == 2);
 
-    return xt::eval(std::sqrt(0.5) * GMatTensor::Cartesian2d::Norm_deviatoric(A));
+    using return_type = typename GMatTensor::allocate<xt::get_rank<T>::value - 2, T>::type;
+    return_type ret = GMatTensor::Cartesian2d::Norm_deviatoric(A);
+    ret *= std::sqrt(0.5);
+    return ret;
 }
 
 /**
@@ -87,7 +90,10 @@ inline auto Sigd(const T& A) -> typename GMatTensor::allocate<xt::get_rank<T>::v
     GMATELASTOPLASTICQPOT_ASSERT(A.shape(A.dimension() - 1) == 2);
     GMATELASTOPLASTICQPOT_ASSERT(A.shape(A.dimension() - 2) == 2);
 
-    return xt::eval(std::sqrt(2.0) * GMatTensor::Cartesian2d::Norm_deviatoric(A));
+    using return_type = typename GMatTensor::allocate<xt::get_rank<T>::value - 2, T>::type;
+    return_type ret = GMatTensor::Cartesian2d::Norm_deviatoric(A);
+    ret *= std::sqrt(2.0);
+    return ret;
 }
 
 /**
